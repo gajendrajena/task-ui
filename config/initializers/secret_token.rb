870832eab64862ed32ceb1a8153150257d9e38fc
gaju <gaju.mca@gmail.com>
2014-01-20 02:52:44 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskUi::Application.config.secret_key_base = '32481a69b8c92efb54ef5226dd5ab760a4dacba6de5b121e161e77c00da2278c126a69b7ba309f8b6ca205dc1daaf7faa6612d63d70a42ed0ddd7c266da63f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaskUi::Application.config.session_store :cookie_store, key: '_task-ui_session'
